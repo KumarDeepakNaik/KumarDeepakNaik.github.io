9ea92e6db6e5a34e0246d573331989a6a65009f9
KumarDeepakNaik <83826009+KumarDeepakNaik@users.noreply.github.com>
2021-05-09 14:55:09 +0530

    Add files via upload

--- js.js
+++ js.js
@@ -16,3 +16,24 @@ head_div_open.style.display="block";
 var head_div_close=document.getElementById("head-div-close");
 head_div_close.style.display="none";
 }
+
+function darkTheme(){
+	var body=document.getElementById("body");
+	body.style.backgroundColor="rgba(0,0,0,1)";
+	body.style.color="rgba(255,255,255,1)";
+	var body_theme_dark=document.getElementById("body-theme-dark");
+	body_theme_dark.style.display="none";
+	var body_theme_dark=document.getElementById("body-theme-light");
+	body_theme_dark.style.display="inline";
+}
+
+function lightTheme(){
+		var body=document.getElementById("body");
+	body.style.backgroundColor="rgba(255,255,255,1)";
+	body.style.color="rgba(0,0,0,1)";
+	var body_theme_dark=document.getElementById("body-theme-dark");
+	body_theme_dark.style.display="inline";
+	var body_theme_dark=document.getElementById("body-theme-light");
+	body_theme_dark.style.display="none";
+	
+}
