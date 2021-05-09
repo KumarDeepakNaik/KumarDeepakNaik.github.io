1f2f448d3eb1b6275eaebe2cc7e2f9e7c8cec6d7
KumarDeepakNaik <83826009+KumarDeepakNaik@users.noreply.github.com>
2021-05-09 13:51:06 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,18 @@
+function navbarOpen(){
+
+var head_table=document.getElementById("head-table");
+head_table.style.display="block";
+var head_div_open=document.getElementById("head-div-open");
+head_div_open.style.display="none";
+var head_div_close=document.getElementById("head-div-close");
+head_div_close.style.display="block";
+}
+function navbarClose(){
+
+var head_table=document.getElementById("head-table");
+head_table.style.display="none";
+var head_div_open=document.getElementById("head-div-open");
+head_div_open.style.display="block";
+var head_div_close=document.getElementById("head-div-close");
+head_div_close.style.display="none";
+}
